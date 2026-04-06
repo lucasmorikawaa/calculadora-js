ff8041c5e074bbcd52ffdc6c27119f6da21db919
Lucas Morikawa <lucasmorikawa23@gmail.com>
2026-04-06 09:40:36 -0300

    closes #4 - Implementando operacoes

--- javascript.js
+++ javascript.js
@@ -1,8 +1,14 @@
-function calc(){
+function calc(operacao){
     var num1 = parseFloat(document.getElementById("num1").value);
     var num2 = parseFloat(document.getElementById("num2").value);
+    var output;
 
-    var output = num1 + num2;
+    switch(operacao){
+        case 'soma' : output = num1 + num2; break;
+        case 'subtracao' : output = num1 - num2; break;
+        case 'multiplicacao' : output = num1 * num2; break;
+        case 'divisao' : output = num1 / num2; break;
+    }
 
     document.getElementById("output").value = output;
 }
